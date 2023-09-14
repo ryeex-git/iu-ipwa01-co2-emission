b182d26a225d9660b0036cb52c9d492828ffc291
ryeex <officialryeex@gmail.com>
2023-09-14 18:44:53 +0200

    Navigation Bar, Fonts, Logo

--- ipwa01-frontend/src/app/app.module.ts
+++ ipwa01-frontend/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ipwa01-frontend/src/app/nav-header/nav-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-header',
+  templateUrl: './nav-header.component.html',
+  styleUrls: ['./nav-header.component.css']
+})
+export class NavHeaderComponent {
+  constructor(private readonly router: Router){}
+}
