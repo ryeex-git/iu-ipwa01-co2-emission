bace5c0d0f7c6003d2b3eceda5d85f63869da7fc
ryeex <officialryeex@gmail.com>
2023-09-19 22:36:40 +0200

    Added backend and added TableData interface to UI

---
+++ ipwa01-frontend/src/assets/models/tableData.model.ts
@@ -0,0 +1,11 @@
+export interface TableData {
+  data: DetailedData[];
+}
+
+interface DetailedData {
+  companyName: string;
+  country: string;
+  emissionInTonnePerYear: number;
+  unitPerYear: number;
+  companyType: string[];
+}
